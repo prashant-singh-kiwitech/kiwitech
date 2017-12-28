4f4f2c53bb8f13ee4bf2aa33045eba26fbc85d71
prashant-singh-kiwitech <prashant.singh.kiwitech@gmail.com>
2017-12-28 15:47:59 +0530

    helloworld

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php namespace Kiwitech\Helloworld;
+
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author yourname
+*/
+class HelloWorld{
+
+  public function hello($param1){
+			return "Hello World";
+   }
+}
